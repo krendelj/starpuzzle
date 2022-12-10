0e7b944e5c695a1f308fb3f7072531b36298bc23
Pavel Andreyev <jarjar@inbox.lv>
2022-12-10 18:40:07 +0200

    Added confetti when the puzzle is solved

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NStarCanvasProperties from './NStarCanvasProperties';
 import NStarPlot from './NStarPlot';
 import PerimeterPlotter from './PerimeterPlotter';
 import SlotsPlotter from './SlotsPlotter';
+import confettiSticker from './confetti-sticker.gif';
 
 class App extends React.Component {
 
@@ -30,7 +31,9 @@ class App extends React.Component {
 	    freeValues: nStarLogic.getValues(),
 	    selectedFreeIndex: -1,
 	    nStarConfiguration: nStarLogic.getEmptyConfiguration(),
-	    selectedNStarIndex: -1
+	    selectedNStarIndex: -1,
+	    allSolved: false,
+	    showConfetti: false,
 	};
     }
 
@@ -72,7 +75,8 @@ class App extends React.Component {
 		freeValues: newFreeValues,
 		selectedFreeIndex: newSelectedFreeIndex,
 		nStarConfiguration: newNStarConfiguration,
-		selectedNStarIndex: newSelectedNStarIndex
+		selectedNStarIndex: newSelectedNStarIndex,
+		...this.setConfetti(newNStarConfiguration, state),
 	    };
 	});
     }
@@ -125,9 +129,27 @@ class App extends React.Component {
 		freeValues: newFreeValues,
 		selectedFreeIndex: newSelectedFreeIndex,
 		nStarConfiguration: newNStarConfiguration,
-		selectedNStarIndex: newSelectedNStarIndex
+		selectedNStarIndex: newSelectedNStarIndex,
+		...this.setConfetti(newNStarConfiguration, state),
 	    };
 	});
+    }
+
+    setConfetti(nStarConfiguration, state) {
+	function checkAllSolved(nStarConfiguration, nStarLogic) {
+	    for (let i = 0; i < nStarLogic.n * 2; i++) {
+		let sum = nStarLogic.getSumForIndexForward(i, nStarConfiguration);
+		if (sum === -1 || sum !== nStarLogic.getDesiredSum())
+		    return false;
+	    }
+	    return true;
+	}
+	
+	let allSolved = checkAllSolved(nStarConfiguration, state.nStarLogic);
+	return {
+	    allSolved: allSolved,
+	    showConfetti: allSolved && !state.allSolved
+	};
     }
 
     selectLang(lang) {
@@ -185,11 +207,36 @@ class App extends React.Component {
     }
 
     render() {
+	let confetti = null, clearConfetti = null;
+	if (this.state.showConfetti) {
+	    confetti = (
+		<img src={confettiSticker} width={this.state.nStarCanvasProperties.width} //height='467.6px'
+		     alt='confetti'
+		     style={{
+			 position: 'absolute',
+			 marginLeft: 'auto',
+			 marginRight: 'auto',
+			 left: 0,
+			 right: 0,
+			 textAlign: 'center',
+			 zIndex: -1,
+		     }} />
+	    );
+	    
+	    clearConfetti = () => {
+		this.setState({
+		    showConfetti: false
+		});
+	    }
+	}
+	
 	return (
-	    <div className="App">
+	    <div className="App" style={{position: 'relative'}} onClick={clearConfetti}>
+		{confetti}
 		<Link to="/">en</Link>&ensp;
 		<Link to="/lv">lv</Link>&ensp;
 		<Link to="/ru">ru</Link>
+		<br />
 		<br />
 		<svg height={this.state.nStarCanvasProperties.height} width={this.state.nStarCanvasProperties.width}>
 		    <ValuesPanel nStarCanvasProperties={this.state.nStarCanvasProperties} freeValues={this.state.freeValues}
@@ -219,7 +266,6 @@ class App extends React.Component {
 		    <br />
 		    {this.resources.theTotalIs} {this.state.nStarLogic.getDesiredSum()}.
 		</div>
-		
 	    </div>
 	);
     }
